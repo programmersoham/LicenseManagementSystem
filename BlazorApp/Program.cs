cde058612abf884a1602d91da7f228545e15541a
programmersohoam <purohit.soham.ps@gmail.com>
2024-01-24 18:23:10 +0530

    added  auth state provider with session storage

--- BlazorApp/Program.cs
+++ BlazorApp/Program.cs
@@ -1,11 +1,18 @@
-var builder = WebApplication.CreateBuilder(args);   
+using BlazorApp.Authentication;
 
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+var builder = WebApplication.CreateBuilder(args);   
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddHttpClient();
 builder.Services.AddHttpContextAccessor();
+builder.Services.AddAuthenticationCore();
 builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<ProtectedSessionStorage>();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
+
 
 var app = builder.Build();
 
